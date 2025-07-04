96d0c4c353d63812aa5f96150ebddb437ea338bd
Sanika2418 <sanikashewale24@gmail.com>
2025-07-04 17:44:44 +0000

    container config done

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountID = 123456    //contanat never change
+let accountEmail = "sanikashewale20@gmail.com"  //
+var accountPassword ="1234"
+accountcity="Jaipur"
+
+console.table([accountID, accountEmail, accountPassword, accountcity])
